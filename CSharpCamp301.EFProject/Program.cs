13b84d68ea1cdb73591ddb327d98a34b2b120255
ihsan <42092819+ihsanfil@users.noreply.github.com>
2024-11-26 12:22:40 +0300

    update

--- CSharpCamp301.EFProject/Program.cs
+++ CSharpCamp301.EFProject/Program.cs
@@ -17,6 +17,7 @@ namespace CSharpCamp301.EFProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmStatistics());
+
         }
     }
 }
